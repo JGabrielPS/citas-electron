d6f8e0b97fdb42caa44a589e822b49f3901ba1b7
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-03-02 17:02:45 -0300

    addition: added all methods to manage paciente

--- API/controllers/pacienteController.js
+++ API/controllers/pacienteController.js
@@ -1,3 +1,61 @@
-exports.nuevoCliente = (req, res, next) => {
-  res.json({mensaje:"El cliente se agrego correctamente"})
-}
+const Paciente = require("../models/Paciente");
+
+exports.nuevoCliente = async (req, res, next) => {
+  const paciente = new Paciente(req.body);
+
+  try {
+    await paciente.save();
+    res.json({ mensaje: "Paciente ingresado correctamente" });
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+};
+
+exports.obtenerPacientes = async (req, res, next) => {
+  try {
+    const pacientes = await Paciente.find({});
+    res.json(pacientes);
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+};
+
+exports.obtenerPaciente = async (req, res, next) => {
+  try {
+    const paciente = await Paciente.findById(req.params.id);
+    res.json(paciente);
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+};
+
+exports.actualizarPaciente = async (req, res, next) => {
+  try {
+    const paciente = await Paciente.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.json(paciente);
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+};
+
+exports.eliminarPaciente = async (req, res, next) => {
+  try {
+    await Paciente.findOneAndDelete({ _id: req.params.id });
+    res.json({
+      mensaje: `El paciente con _id: ${req.params.id} fue eliminado`,
+    });
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+};
