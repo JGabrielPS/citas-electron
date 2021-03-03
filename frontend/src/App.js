6f52cd0299d2f8d02338fadaec43543607255743
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-03-03 18:11:31 -0300

    addition: added consultar useState to control reload of citas to show, added the corresponding element in useEffect (and a second argument [consultar] so that it observe changes in that state). Also added that hook in Nueva Cita and Cita (plus all the logic to render it)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,23 +9,43 @@ import Cita from "./components/Cita";
 
 function App() {
   const [citas, guardarCitas] = useState([]);
+  const [consultar, guardarConsultar] = useState(true);
 
   useEffect(() => {
-    const consultarAPI = () => {
-      clienteAxios
-        .get("/pacientes")
-        .then((resp) => guardarCitas(resp.data))
-        .catch((error) => console.log(error));
-    };
-    consultarAPI();
-  }, []);
+    if (consultar) {
+      const consultarAPI = () => {
+        clienteAxios
+          .get("/pacientes")
+          .then((resp) => {
+            guardarCitas(resp.data);
+
+            guardarConsultar(false);
+          })
+          .catch((error) => console.log(error));
+      };
+      consultarAPI();
+    }
+  }, [consultar]);
 
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={() => <Pacientes citas={citas} />} />
-        <Route exact path="/nueva" component={NuevaCita} />
-        <Route exact path="/cita/:id" component={Cita} />
+        <Route
+          exact
+          path="/nueva"
+          component={() => <NuevaCita guardarConsultar={guardarConsultar} />}
+        />
+        <Route
+          exact
+          path="/cita/:id"
+          render={(props) => {
+            const cita = citas.filter(
+              (cita) => cita._id === props.match.params.id
+            );
+            return <Cita cita={cita[0]} guardarConsultar={guardarConsultar} />;
+          }}
+        />
       </Switch>
     </Router>
   );
